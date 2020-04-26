48325a0f327a472132e8cde7e2edbf34f0504170
wsaldana <53311124+wsaldana@users.noreply.github.com>
2020-04-25 22:15:54 -0600

    Se agregó implementación Splay

--- src java/Factory.java
+++ src java/Factory.java
@@ -5,19 +5,19 @@
 *Factory para instanciar el tree/map
 *
 *@author Andrea Amaya #19357
-*@author Walter Saldana #19357
+*@author Walter Saldana #19897
 **********************************************************/
-public class Factory{
+public class Factory<K extends Comparable<K>,V>{
 	/**
 	//Pre: Haber elegido un traductor 
 	//Post: Intancia de tipo de tranductor seleccionado
 	 * @param tipoTranslator   Tipo de translator a instanciar
 	*/
-	public TranslatorInterface getTranslator(int tipoTranslator){
+	public TranslatorInterface<K, V> getTranslator(int tipoTranslator){
 		if (tipoTranslator == 1) { //Es un hash map
-			return new Hashing();
+			return new Hashing<K,V>();
 		} else { //Es un splay tree
-			return new Hashing();
+			return new MapSplayTree<K,V>();
 		}
 	}
 }

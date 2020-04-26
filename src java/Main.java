0a318d7f4cac27d3f1a3214d9ef828cae61725fd
wsaldana <53311124+wsaldana@users.noreply.github.com>
2020-04-25 22:17:23 -0600

    Traslado de métodos a clase controladora

--- src java/Main.java
+++ src java/Main.java
@@ -5,90 +5,54 @@
 *Clase principal para interactuar con el usuario
 *
 *@author Andrea Amaya #19357
-*@author Walter Saldana #19
+*@author Walter Saldana #19897
 **********************************************************/
-import java.util.*;
-import java.io.*; 
+
+import java.io.File;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 
 public class Main{
 	public static void main(String[] args) {
-		TranslatorInterface dictionary; //Espacio para el diccionario
-		ArrayList<String> text = new ArrayList<String>(); //Texto
 
-		dictionary = new Hashing();
+		Factory<String,String> factory = new Factory<>();
+		TranslatorInterface<String,String> dictionary = null;
 
-		//Se leen ambos txt
-		try {
-			Scanner r = new Scanner(new File("Spanish.txt")); 
-			while (r.hasNextLine()) {
-				String txt = r.nextLine(); //Mientras hayan lineas por leer se meten a la variable txt
-				String[] word = txt.split("	"); 
-
-				if(word.length != 2 ){
-					word = txt.split("\\s+");
-				} 
-
-
-				try{
-					try {
-						String[] posible = word[1].split(",");
-						dictionary.put(new Association(word[0], posible[0])); //Se agrega
-					} catch (Exception e) {
-						//No tiene varios significados en la misma linea
-						dictionary.put(new Association(word[0], word[1])); //Se agrega
-					}
-					
-				} catch (Exception e) {
-					//Ya existe la palabra en el diccionario (llave repetida)
-				}
-				
+		//Solicitar al usuario la implementacion hasta que la seleccione correctamente
+		while(dictionary == null){
+			String implementacion = JOptionPane.showInputDialog(null, "Ingrese '1' para implementar un HASHING \nIngrese '2' para implementar SPLAY TREE");
+			if(implementacion.equals("1")){
+				dictionary = factory.getTranslator(1); //Usar implmentacion de hashing
+			}else if(implementacion.equals("2")){
+				dictionary = factory.getTranslator(2); //Usar implementacion de SplayTree
+			}else{
+				JOptionPane.showMessageDialog(null, "Por favor ingrese una opcion valida >:(");
 			}
-			r.close();	
-		} catch (Exception e) { //Se muestra la razon de error por la que no se encuentra el doc
-			e.printStackTrace();
 		}
 
-		try {
-			Scanner r = new Scanner(new File("texto.txt")); 
-			while (r.hasNextLine()) {
-				String txt = r.nextLine(); //Mientras hayan lineas por leer se meten a la variable txt
-				txt = txt.replace(".", ""); 
-				String[] word = txt.split(" "); 
+		//Manejar visualizador de archivos
+		JFileChooser jfc = new JFileChooser();
+		jfc.setCurrentDirectory(new File(System.getProperty("user.dir")));
 
-				for (int i=0; i<word.length; i++) {
-					text.add(word[i]); //Se agrega al texto	
-				}
-				
-			}
-			r.close();	
-		} catch (Exception e) { //Se muestra la razon de error por la que no se encuentra el doc
-			e.printStackTrace();
-		}
+		//Solicitar archivo de traducciones
+		JOptionPane.showMessageDialog(null, "A continuacion ingerse un archivo con traducciones");
+		jfc.showOpenDialog(null); 
+		File file = jfc.getSelectedFile();
 
-		System.out.println(text); //Antes de la traduccion
+		//Solicitar archivo con frase a traducir
+		JOptionPane.showMessageDialog(null, "A continuacion ingerse un archivo con la frase a traducir");
+		jfc.showOpenDialog(null); 
+		File file2 = jfc.getSelectedFile();
 
-		//Por cada palabra en el texto/
-		for (int i=0; i<text.size(); i++) {
-			String actual = text.get(i).toLowerCase();
-			//Por cada palabra en el diccionario
-			for (int j=0; j<dictionary.size(); j++) {
-				//Si la palabra del diccionario es igual a la palabra actual del texto
-				if (dictionary.searchValue(actual)) {
-					if (i+1 == text.size()) {
-						text.set(i,dictionary.get(actual).toString()+"."); //Se vuelve a colocar el punto
-					} else{
-						text.set(i,dictionary.get(actual).toString()); //Se coloca la palabra
-					}
-					
-					break;
-				}
-			}	
-		}
+		//Realizar traduccion
+		Translator translator = new Translator(file,file2,dictionary);
+		translator.agregarDiccionario();
+		String oracion = translator.getOracion();
+		String traduccion = translator.traducir();
 
-		System.out.println(text); //Despues de la traduccion
-
-
-
+		//Mostrar resultados
+		System.out.println(oracion+"\n"+traduccion);
+		JOptionPane.showMessageDialog(null, oracion+"\nSe traduce como: \n"+traduccion);
 	}
 }
 

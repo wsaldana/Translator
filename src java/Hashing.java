04ffed4ce95d33e72b989b58bb9ea957a176aec2
wsaldana <53311124+wsaldana@users.noreply.github.com>
2020-04-25 22:16:18 -0600

    SETTERS Y GETTERS!!!!!!!!

--- src java/Hashing.java
+++ src java/Hashing.java
@@ -5,40 +5,46 @@
 *Clase encargada de los metodos de un mapa hash
 *
 *@author Andrea Amaya #19357
-*@author Walter Saldana #19
+*@author Walter Saldana #19897
 *
 *Codigo adaptado de: Java Platform SE 8
 https://docs.oracle.com/javase/8/docs/api/java/util/HashMap.html
 **********************************************************/
 import java.util.HashMap;
-import java.lang.*;
 
 public class Hashing<K extends Comparable<K>, V> implements TranslatorInterface<K,V>{
-	protected HashMap<K, V> data  = new HashMap<K, V>(); // datos agregados
+	private HashMap<K, V> data ; // datos agregados
 
+	public Hashing(){
+		this.data = new HashMap<K, V>();
+	}
+
+	@Override
 	public V remove(K key){
 		return data.remove(key);
 	}
 
-
-	public V put(Association association){
+	@Override
+	public V put(Association<K,V> association){
 		return data.put((K)association.getKey(), (V)association.getValue());
 	}
 
-
+	@Override
 	public int size(){
 		return data.size();
 	}
 
-	public V get(Object key){
-		return data.get(key);
-	}
-
-	public boolean searchValue(String searched){
+	@Override
+	public boolean searchValue(K searched) {
 		if (data.containsKey(searched)) {
 			return true;
 		}
 		return false;
+	}
+
+	@Override
+	public V get(K key) {
+		return data.get(key);
 	}
 
 }
